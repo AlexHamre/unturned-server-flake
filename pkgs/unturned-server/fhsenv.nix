a1a723275edda7efc610a16708fdd18c103f9b72
alexhamre <alexhamre>
2025-02-02 15:30:09 +0100

    serverhelper has term

--- pkgs/unturned-server/fhsenv.nix
+++ pkgs/unturned-server/fhsenv.nix
@@ -12,6 +12,7 @@ buildFHSUserEnv {
   runScript = writeScript "unturned-server-wrapper" ''
     export LD_LIBRARY_PATH=${steamworks-sdk-redist}/lib:$LD_LIBRARY_PATH
     export SteamAppId=892970
+    export TERM=xterm
     exec ${unturned-server-unwrapped}/Unturned_Headless.x86_64 -batchmode -nographics +LanServer/MyServer "$@"
   '';
 
