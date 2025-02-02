7b7ec38bb5c2f29c3fed84d9ae6c2c0c39c9f73d
alexhamre <alexhamre>
2025-02-02 13:34:28 +0100

    fix?

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
     steam-fetcher = {
-      url = "github:aidalgol/nix-steam-fetcher";
+      url = "github:AlexHamre/steam-fetcher";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,9 +12,48 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     steam-fetcher,
-  }: {
+  }: let
+    # The Steam Nix fetcher only supports x86_64 Linux.
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          steam-fetcher.overlays.default
+          self.overlays.default
+        ];
+      };
+    lintersFor = system: let
+      pkgs = pkgsFor system;
+    in
+      with pkgs; [
+        alejandra
+      ];
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            nil # Nix LS
+          ]
+          ++ lintersFor system;
+      };
+    });
+
+    checks = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      fmt = pkgs.runCommandLocal "alejandra" {} ''
+        ${pkgs.alejandra}/bin/alejandra --check ${./.} > "$out"
+      '';
+    });
+
+    formatter = forAllSystems (system: (pkgsFor system).alejandra);
+
     nixosModules = rec {
       unturned = import ./nixos-modules/unturned.nix {inherit self steam-fetcher;};
       default = unturned;
@@ -23,7 +61,12 @@
     overlays.default = final: prev: {
       unturned-server-unwrapped = final.callPackage ./pkgs/unturned-server {};
       unturned-server = final.callPackage ./pkgs/unturned-server/fhsenv.nix {};
-      # steam-run = nixpkgs.steam-run;
     };
+    packages = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      unturned-server = pkgs.unturned-server;
+      default = pkgs.unturned-server;
+    });
   };
 }
