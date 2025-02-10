c15073df2b8bb67a691e65490b41d77f780df7d7
alexhamre <alexhamre>
2025-02-10 17:36:52 +0100

    missing many files?

--- pkgs/unturned-server/default.nix
+++ pkgs/unturned-server/default.nix
@@ -2,36 +2,29 @@
   lib,
   stdenv,
   fetchSteam,
+  symlinkJoin,
 }:
-stdenv.mkDerivation rec {
-  name = "unturned-server";
-  version = "3.24.7.1";
-  src = fetchSteam {
-    inherit name;
+
+let
+  baseDepot = fetchSteam {
+    name = "unturned-server";
     appId = "1110390";
     depotId = "1110392";
     manifestId = "3114215424494400556";
     hash = "sha256-mzQm21J1p318Bq7Mhfp5VlqxaEFejM1SA3HOwlYUfSA=";
   };
 
-  # Skip phases that don't apply to prebuilt binaries.
-  dontBuild = true;
-  dontConfigure = true;
-  dontFixup = true;
+  extraDepot = fetchSteam {
+    name = "unturned-server";
+    appId = "1110390";
+    depotId = "1110393"; # Replace with the correct depot ID
+    manifestId = "3381894269381868866"; # Replace with the correct manifest ID
+    hash = "sha256-HJpRCQDr6ss+Zi6PdXaic6bZzmjfHAgN652CKbBLMfM="; # Replace with the correct hash
+  };
 
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out
-    cp -r \
-      * \
-      $out
-
-    # You may need to fix permissions on the main executable.
-    chmod +x $out/Unturned_Headless.x86_64
-
-    runHook postInstall
-  '';
+in symlinkJoin {
+  name = "unturned-server";
+  paths = [ baseDepot extraDepot ];
 
   meta = with lib; {
     description = "Unturned dedicated server";
